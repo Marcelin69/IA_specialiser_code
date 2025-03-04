66ba48648ce18f992defffd36dd37228d1596402
Marcelin <marcelintingougoui3@gmail.com>
2025-03-04 12:29:31 +0100

    modif1

--- IA_avance_5/app.py
+++ IA_avance_5/app.py
@@ -1,4 +1,6 @@
+import numpy as np 
 from PIL import Image
+import cv2
 # TensorFlow and tf.keras
 from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
